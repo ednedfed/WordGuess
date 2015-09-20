4efc47836c25c288b3cb969bf6029c2f01beea26
ednedfed <realvictory@yahoo.com>
2015-09-21 00:23:30 +0100

    missed file

--- WordGuessing/Assets/IngameObjects/DraggableObject.cs
+++ WordGuessing/Assets/IngameObjects/DraggableObject.cs
@@ -10,84 +10,44 @@ public class DraggableObject : MonoBehaviour
 
 	public char character;
 
-	protected Slot _mySlot;
-
-	protected SlotPool _slotPool;
+	protected SlotPool _tileSlotPool;
 	protected SlotPool _answerSlotPool;
 
 	protected SlotPool _currentSlotPool;
 	protected SlotPool _nextSlotPool;
-
-	protected event Action<DraggableObject> _onLetterAdded;
-	protected event Action<DraggableObject> _onLetterRemoved;
-
+	
 	void Awake()
 	{
 		Text myText = GetComponentInChildren<Text>();
 		character = char.Parse(myText.text);
 	}
 
-	public void Register(Action<DraggableObject> onLetterAdded, Action<DraggableObject> onLetterRemoved)
+	public void SetSlotPools(SlotPool tileSlotPool, SlotPool answerSlotPool)
 	{
-		_onLetterAdded += onLetterAdded;
-		_onLetterRemoved += onLetterRemoved;
-	}
-
-	public void Unregister(Action<DraggableObject> onLetterAdded, Action<DraggableObject> onLetterRemoved)
-	{
-		_onLetterAdded -= onLetterAdded;
-		_onLetterRemoved -= onLetterRemoved;
-	}
-
-	public void SetSlot(Slot slot)
-	{
-		_mySlot = slot;
-	}
-
-	public void SetSlotPools(SlotPool slotPool, SlotPool answerSlotPool)
-	{
-		_slotPool = slotPool;
+		_tileSlotPool = tileSlotPool;
 		_answerSlotPool = answerSlotPool;
 
-		_currentSlotPool = _slotPool;
+		_currentSlotPool = _tileSlotPool;
 		_nextSlotPool = _answerSlotPool;
+
+		_currentSlotPool.AddToNextFreeSlot(this);
 	}
 
 	protected void SwapCurrentSlotPool()
 	{
+		if(_nextSlotPool.IsFull())
+			return;
+		
+		_currentSlotPool.RemoveFromSlot(this);
+		_nextSlotPool.AddToNextFreeSlot(this);
+
 		SlotPool tempPool = _currentSlotPool;
 		_currentSlotPool = _nextSlotPool;
 		_nextSlotPool = tempPool;
-
-		if(_currentSlotPool == _answerSlotPool)
-		{
-			if(_onLetterAdded != null)
-				_onLetterAdded(this);
-		}
-		else
-		{
-			if(_onLetterRemoved != null)
-				_onLetterRemoved(this);
-		}
 	}
 
 	protected virtual void OnMouseDown()
 	{
-		//Debug.Log("On Mouse Up");
-		if(_nextSlotPool.IsFull())
-			return;
-
-		_currentSlotPool.SetFree(_mySlot);
-
-		_mySlot = _nextSlotPool.GetNextFreeSlot();
-
-		_nextSlotPool.SetUsed(_mySlot);
-
 		SwapCurrentSlotPool();
-
-		Vector3 newPos = _mySlot.transform.position;
-		newPos.z = -1f;
-		
-		transform.position = newPos;
 	}
 }
